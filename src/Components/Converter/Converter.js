dca74588fee60841617732c99007fbd4ce74f398
kds <kdskoudous98@gmail.com>
2022-09-09 11:28:59 +0100

    update converter js

--- src/Components/Converter/Converter.js
+++ src/Components/Converter/Converter.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAppContext } from '../../App/AppContext';
 import './converter.scss';
 
